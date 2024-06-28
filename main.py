99cf5b4b14c711631c90c27697ed62347423d1b3
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2024-06-28 20:00:41 -0300

    Update grafica casi terminada

--- main.py
+++ main.py
@@ -3,15 +3,60 @@ import asyncio
 import aiohttp
 
 async def main(page: ft.Page):
+    
+    # Variable para controlar si se muestra o no el botón EMPEZAR
+    show_button = True
+    
+    # Variable para rastrear si el botón EMPEZAR fue presionado
+    button_pressed = False
+    
+    # Variable para rastrear si el botón REINICIO fue presionado
+    buttonpressed = False
+    
     #funcion para cerrar la pag 
     def close_page(e):
         page.clean()
         page.go("/")
         
     #Funcion de ejemplo pata mostrar el ID de los botones seleccionados
-    def on_button_click(e):
+    def on__click(e):
         print(f"Botón con ID {e.control.data} presionado")
         
+    def on_reinicio(e):
+        nonlocal buttonpressed
+        print(f"Botón con ID {e.control.data} presionado")
+        buttonpressed = True
+        #volver_inicio(page) <-- Funcion para volver todo a los valores predeterminados ya la pag principal
+    
+    #Funcion para el boton del comienzo
+    def on_button_click(e):
+        nonlocal show_button
+        nonlocal button_pressed
+            
+        print(f"Botón con ID {e.control.data} presionado")
+        show_button = False
+        button.visible = False
+        button_pressed = True
+        update_screen(page)
+        
+        #funcion para que cambie la pantalla una vez inicad
+        # Función para cambiar lo que se muestra en la pantalla según el valor de button_pressed
+    def update_screen(page):
+        print(button_pressed)
+        print("Entro a la funcion")
+        if button_pressed == True:
+            print("Entro al if")
+            pantallaCentral.content = ft.Image(
+                src="Imagenes/tamagotchi_huevo.gif",
+                scale=0.5,
+            )
+        else:
+            print("Entro al else")
+            pantallaCentral.content =ft.Image(
+                src="Imagenes/tamagotchi.png",
+                scale=0.5,
+            )
+        page.update()
 
     #Caracteristicas de la ventana
     page.window_width = 400
@@ -28,6 +73,7 @@ async def main(page: ft.Page):
     negro = '0xFF000000'
     blanco = '0xFFFFFFF'
     blanco24 = '0x3DFFFFFF'
+    verdelima = '#00FF00'
 
     #Imagenes
     imagenLogo = ft.Image (
@@ -36,10 +82,34 @@ async def main(page: ft.Page):
         height = 60,
         fit = ft.ImageFit.CONTAIN,
     )
-
-    imagenTamagotchi = ft.Image (
-        src = "Imagenes/tamagotchi.png",
-        scale = 0.5,      
+    
+    #Boton de empezar se muestra al inicio unicamente
+    button = ft.ElevatedButton(
+        text="Empezar",
+        height=50,
+        width=150,
+        data="BotonEmpezar",
+        style=ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=10),
+        ),
+        bgcolor= verdelima,
+        color=ft.colors.BLACK,
+        on_click=on_button_click,
+        visible=show_button,
+    )
+    
+    #boton de reinicio aparece a los 40 segundos de apretar el boton de empezar
+    buttonReinicio = ft.ElevatedButton(
+        content=ft.Icon(ft.icons.RESTART_ALT, color=ft.colors.WHITE),
+        width = 50,
+        height = 50,
+        data="BotonReinicio",
+        style=ft.ButtonStyle(
+            shape=ft.CircleBorder(),
+            bgcolor= ft.colors.BLUE,
+            side=ft.BorderSide(width=2, color=blanco),
+        ),
+        on_click=on_reinicio,        
     )
 
     # Botones (Salir, Flechas, Circulo)
@@ -53,7 +123,6 @@ async def main(page: ft.Page):
             shape=ft.CircleBorder(),  
             side = ft.BorderSide(width=2, color=blanco),
         )
-        
     )
 
     # Diseño de la parte superior, se agrega el boton y la imagen de logo
@@ -67,8 +136,13 @@ async def main(page: ft.Page):
         ft.Container(
             imagenLogo,
             alignment=ft.alignment.center,
-            expand= True  # Permite que el contenedor de la imagen se expanda para centrar la imagen
+            expand= True # Permite que el contenedor de la imagen se expanda para centrar la imagen
         ),
+        ft.Container(
+            buttonReinicio,
+            width = 80,
+            height = 45,
+        )
     ]
 
     pantallaCentral = ft.Stack ([
@@ -78,12 +152,11 @@ async def main(page: ft.Page):
             bgcolor = blanco24,
             border_radius = 20,
             border = ft.border.all(4, blanco),
-            content = ft.Container(
-                imagenTamagotchi,
-                alignment= ft.alignment.center,
-                scale=1,
+            content=ft.Image(
+                src="Imagenes/tamagotchi.png",
+                scale=0.5,
             ),
-            alignment = ft.alignment.center
+            alignment=ft.alignment.center,
         )
     ])
 
@@ -120,15 +193,17 @@ async def main(page: ft.Page):
                         #Boton1
                         ft.ElevatedButton(text="<",height=50,width=50,data="BotonIzquierdo",style=ft.ButtonStyle(
                             shape=ft.CircleBorder(),  
-                        ),color=ft.colors.BLACK,on_click=on_button_click),
+                        ),bgcolor=ft.colors.AMBER,color=ft.colors.BLACK,on_click=on__click),
                         #Boton2
                         ft.ElevatedButton(height=50,width=50,data="BotonDelMedio",style=ft.ButtonStyle(
                             shape=ft.CircleBorder(),
-                        ),color=ft.colors.BLACK,on_click=on_button_click),
+                        ),bgcolor=ft.colors.AMBER,color=ft.colors.BLACK,on_click=on__click),
                         #Boton3
                         ft.ElevatedButton(text=">",height=50,width=50,data="BotonDerecho",style=ft.ButtonStyle(
                             shape=ft.CircleBorder(),  
-                        ),color=ft.colors.BLACK,on_click=on_button_click),
+                        ),bgcolor=ft.colors.AMBER,color=ft.colors.BLACK,on_click=on__click),
+                        #boton que aparece una unica vez
+                        button,
                     ]
             )
             #border = ft.border.all() <-- margen para la creacion del diseño
@@ -159,4 +234,7 @@ async def main(page: ft.Page):
     )
 
     await page.add_async(contenedor)
+    
+    # Mostrar la pantalla inicial
+    update_screen(page)
 ft.app(target=main, port=5050)
