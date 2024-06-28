4841d1875e47ae17c43094f4eeb13b431ac25182
Jessica Pagano <jeessieh@hotmail.com>
2024-06-28 01:41:17 -0300

    main.py
    CLASE TAMAGOTCHI

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+#SE CREA CLASE - STATS EN CERO(INICIALIZACION EN CERO) - - ¿AGREGAR ALGO MAS?
+class Tamagotchi:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.hambre = 0
+        self.aburrimiento = 0
+        self.sueno = 0
+
+    def alimentar(self):
+        if self.hambre > 0:
+            self.hambre -= 1
+            print(f"Alimentaste a {self.nombre}. Hambre: {self.hambre}")
+        else:
+            print(f"{self.nombre} no tiene hambre.")
+
 #Se crea un bucle que permitirá al usuario interactuar con el Tamagotchi.
 #MENU INTERACTIVO EN DONDE EL USUARIO ELIGE QUE HACER
 def main():
